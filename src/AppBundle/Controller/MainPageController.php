a8b491f2cd2a016aa05d20c4677dba0b9b5255a5
abryb <andrzej.blazej.rybarkiewicz@gmail.com>
2017-05-10 10:52:51 +0200

    Create CRUD Offer and MainPageController

---
+++ src/AppBundle/Controller/MainPageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MainPageController extends Controller
+{
+    /**
+     * @param
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('base.html.twig', array('name' => 'name'));
+    }
+}
