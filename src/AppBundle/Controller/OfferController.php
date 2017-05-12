cab9a4fd9abe7163137c56222a9eadaeb3678779
abryb <andrzej.blazej.rybarkiewicz@gmail.com>
2017-05-12 10:49:15 +0200

    move offer new, edit, delete to UserOffer Controller

--- src/AppBundle/Controller/OfferController.php
+++ src/AppBundle/Controller/OfferController.php
@@ -56,35 +56,6 @@ class OfferController extends Controller
     }
 
     /**
-     * Creates a new offer entity.
-     *
-     * @Route("/new", name="offer_new")
-     * @Method({"GET", "POST"})
-     * @Security("has_role('ROLE_USER')")
-     */
-    public function newAction(Request $request)
-    {
-        $user = $this->getUser();
-        $offer = new Offer();
-        $form = $this->createForm('AppBundle\Form\OfferType', $offer);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $offer->setUser($user);
-            $em->persist($offer);
-            $em->flush();
-
-            return $this->redirectToRoute('offer_show', array('id' => $offer->getId()));
-        }
-
-        return $this->render('offer/new.html.twig', array(
-            'offer' => $offer,
-            'form' => $form->createView(),
-        ));
-    }
-
-    /**
      * Finds and displays a offer entity.
      *
      * @Route("/{id}", name="offer_show")
@@ -95,71 +66,5 @@ class OfferController extends Controller
         return $this->render('offer/show.html.twig', array(
             'offer' => $offer,
         ));
-    }
-
-    /**
-     * Displays a form to edit an existing offer entity.
-     *
-     * @Route("/{id}/edit", name="offer_edit")
-     * @Method({"GET", "POST"})
-     */
-    public function editAction(Request $request, Offer $offer)
-    {
-        $deleteForm = $this->createDeleteForm($offer);
-        $editForm = $this->createForm('AppBundle\Form\OfferType', $offer);
-        $editForm->handleRequest($request);
-
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('offer_edit', array('id' => $offer->getId()));
-        }
-
-        return $this->render('offer/edit.html.twig', array(
-            'offer' => $offer,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-     * Deletes a offer entity.
-     *
-     * @Route("/{id}", name="offer_delete")
-     * @Method("DELETE")
-     */
-    public function deleteAction(Request $request, Offer $offer)
-    {
-        $form = $this->createDeleteForm($offer);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($offer);
-            $em->flush();
-        }
-
-        return $this->redirectToRoute('offer_index');
-    }
-
-    /**
-     * Creates a form to delete a offer entity.
-     *
-     * @param Offer $offer The offer entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(Offer $offer)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('offer_delete', array('id' => $offer->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
-        ;
-    }
-
-    public function getUser()
-    {
-        return parent::getUser();
     }
 }
--- src/AppBundle/Controller/UserOfferController.php
+++ src/AppBundle/Controller/UserOfferController.php
@@ -3,9 +3,11 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Offer;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
 
 
 /**
@@ -34,9 +36,10 @@ class UserOfferController extends Controller
     }
 
     /**
-     * @Route("/{id}")
+     * @Route("/{id}", requirements={"id":"\d+"})
      * @param Offer $offer
      * @return \Symfony\Component\HttpFoundation\Response
+     * @Method("GET")
      */
     public function showAction(Offer $offer)
     {
@@ -47,10 +50,84 @@ class UserOfferController extends Controller
         ));
     }
 
+    /**
+     * Displays a form to edit an existing offer entity.
+     *
+     * @Route("/{id}/edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Offer $offer)
+    {
+        $deleteForm = $this->createDeleteForm($offer);
+        $editForm = $this->createForm('AppBundle\Form\OfferType', $offer);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('offer_edit', array('id' => $offer->getId()));
+        }
+
+        return $this->render('offer/edit.html.twig', array(
+            'offer' => $offer,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a offer entity.
+     *
+     * @Route("/{id}")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Offer $offer)
+    {
+        $form = $this->createDeleteForm($offer);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($offer);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('app_useroffer_index');
+    }
+
+    /**
+     * Creates a new offer entity.
+     *
+     * @Route("/new")
+     * @Method({"GET", "POST"})
+     * @Security("has_role('ROLE_USER')")
+     */
+    public function newAction(Request $request)
+    {
+        $user = $this->getUser();
+        $offer = new Offer();
+        $form = $this->createForm('AppBundle\Form\OfferType', $offer);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $offer->setUser($user);
+            $em->persist($offer);
+            $em->flush();
+
+            return $this->redirectToRoute('app_useroffer_show', array('id' => $offer->getId()));
+        }
+
+        return $this->render('offer/new.html.twig', array(
+            'offer' => $offer,
+            'form' => $form->createView(),
+        ));
+    }
+
     private function createDeleteForm(Offer $offer)
     {
         return $this->createFormBuilder()
-            ->setAction($this->generateUrl('offer_delete', array('id' => $offer->getId())))
+            ->setAction($this->generateUrl('app_useroffer_delete', array('id' => $offer->getId())))
             ->setMethod('DELETE')
             ->getForm()
             ;
