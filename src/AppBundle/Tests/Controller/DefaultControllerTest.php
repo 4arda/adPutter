3951f2542903ad9092f311a066c600778fb0f58e
abryb <andrzej.blazej.rybarkiewicz@gmail.com>
2017-05-16 23:47:30 +0200

    instal phpunit bridge, update composer

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -4,6 +4,7 @@ namespace AppBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+
 class DefaultControllerTest extends WebTestCase
 {
     public function testIndex()
@@ -13,6 +14,5 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
 }
